2d9bea7c3aea70f4322ddd5ce835f82843949336
ajaykantkanojiya <ajaykantkanojiya@gmail.com>
2022-01-03 10:25:31 +0530

    add apiphp api controller

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('logi', [Apicontroller::class, 'index'])->name('logi');
+Route::post('loginDriver', [Apicontroller::class, 'loginDriver'])->name('loginDriver');
+Route::get('getAllDrivers', [Apicontroller::class, 'getAllDrivers'])->name('getAllDrivers');
+Route::get('getAllTrucks', [Apicontroller::class, 'getTurck'])->name('getTurck');
+Route::get('getAllTruckAccesories', [Apicontroller::class, 'getAllTruckAccesories'])->name('getAllTruckAccesories');
+Route::post('createMorningAccepted', [Apicontroller::class, 'createMorningAccepted'])->name('createMorningAccepted');
+Route::post('getDriverAssignToLoader', [Apicontroller::class, 'getDriverAssignToLoader'])->name('getDriverAssignToLoader');
+Route::post('getJobsByLoadContainer', [Apicontroller::class, 'getJobsByLoadContainer'])->name('getJobsByLoadContainer');
+
+Route::post('getCustomerDetails', [Apicontroller::class, 'getCustomerDetails'])->name('getCustomerDetails');
+Route::get('allToolType', [Apicontroller::class, 'allToolType'])->name('allToolType');
+
+
+
 // Route::group(['middleware' => ['web']], function () {
    
 // });
